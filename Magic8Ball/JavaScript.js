589544053f717880b5226ffac8aa9e5b6c285d6b
ChristopherMBetteley <Christopher.M.Betteley@gmail.com>
2016-10-19 17:50:30 -0400

    first commit

---
+++ Magic8Ball/JavaScript.js
@@ -0,0 +1,5 @@
+﻿function Shake() {
+        var fortunes = ["try again later", "Most Definately", "NO"]
+        var num = Math.floor((Math.random() * 3) + 1);
+        document.getElementById("Fortune").innerHTML = fortunes[num];
+}
